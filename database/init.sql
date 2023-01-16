1a557c6799ea49b7ae41eb5dcd2c02d23ad7d588
Daan <daanvanemden2@live.nl>
2023-01-17 00:12:14 +0100

    added threads, updated rest and front-end

--- database/init.sql
+++ database/init.sql
@@ -2,3 +2,10 @@ create table messages(
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL
 );
+
+create table threads(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    thread_msgs TEXT[]
+);
+
--- rest/rest.py
+++ rest/rest.py
@@ -20,7 +20,7 @@ api = Flask(__name__)
 cors = CORS(api)
 api.config['CORS_HEADERS'] = 'Content-Type'
 
-@api.route('/', methods=['GET'])
+@api.route('/get_messages', methods=['GET'])
 @cross_origin()
 def get_messages():
     cursor.execute('select * from messages')
@@ -29,14 +29,78 @@ def get_messages():
         messages.append({'id': m[0], 'content': m[1]})
     return jsonify(messages)
 
-@api.route('/new', methods=['POST'])
+
+@api.route('/get_threads', methods=['GET'])
+@cross_origin()
+def get_threads():
+    cursor.execute('select * from threads')
+    threads = []
+    for m in cursor.fetchall():
+        threads.append({'id': m[0], 'name': m[1]})
+    return jsonify(threads)
+
+
+@api.route('/get_thread_msgs', methods=['GET'])
+@cross_origin()
+def get_thread_msgs():
+    print('get_thread_msgs : ', request.values)
+    name = request.values.get('name')
+    cursor.execute('select * from threads where name = %s', (name, ))
+    conn.commit()
+
+    thread_msgs = []
+    for m in cursor.fetchall():
+        print('get_thread_msgs, m[2] = ', m[2])
+        thread_msgs.append({'thread_msgs' : m[2]})
+
+    # 1 / 10 times fetchall doesnt return anything in case of newly 
+    # created threads. Check if msgs are saved in the thread_msgs 
+    # column of these bugged threads 
+
+    # if len(thread_msgs) == 0:
+        # thread_msgs.append({'thread_msgs': None})
+
+    print(thread_msgs)
+
+    return jsonify(thread_msgs)
+
+
+@api.route('/new_message', methods=['POST'])
 @cross_origin()
 def post_message():
     print(request.values)
     content = request.values.get('content')
     cursor.execute("INSERT INTO messages (content) VALUES(%s)", (content, ))
     conn.commit()
-    return redirect("/", code=302)
+    return redirect("/get_messages", code=302)
+
+
+@api.route('/new_thread', methods=['POST'])
+@cross_origin()
+def post_new_thread():
+    name = request.values.get('name')
+    cursor.execute("INSERT INTO threads (name) VALUES(%s)", (name, ))
+    conn.commit()
+    return redirect("/get_threads", code=302)
+
+
+@api.route('/new_thread_msg', methods=['POST'])
+@cross_origin()
+def post_new_thread_msg():
+    print("new_thread_msg, request values : ", request.values)
+    name = request.values.get('name')
+    msg = request.values.get('msg')
+    print('name : ', name, ' msg : ', msg)
+
+    sql = """ UPDATE threads    
+            SET thread_msgs = thread_msgs || %s
+            WHERE name = %s"""
+
+    cursor.execute(sql, ([msg], name))
+    conn.commit()
+
+    return 'New thread msg url'
+
 
 if __name__ == '__main__':
     api.run(host='0.0.0.0', port=1234)
