3672aad8cf51f7c607969ec47421fd3afce17bfb
Daan <daanvanemden2@live.nl>
2023-02-01 09:30:46 +0100

    added rbac roles

---
+++ rbac/init_rbac.sh
@@ -0,0 +1,13 @@
+# make sure that your hostname does not contain  capital 
+# letters or underscores if node is not ready when
+# RBAC is enabled (see https://microk8s.io/docs/troubleshooting, common issues)
+
+microk8s enable rbac
+microk8s kubectl apply -f standard-user-cr.yaml 
+microk8s kubectl apply -f standard-user-crb.yaml 
+microk8s kubectl apply -f deployment-manager-cr.yaml 
+microk8s kubectl apply -f deployment-manager-crb.yaml 
+microk8s kubectl apply -f superuser-cr.yaml 
+microk8s kubectl apply -f superuser-crb.yaml 
+
+
