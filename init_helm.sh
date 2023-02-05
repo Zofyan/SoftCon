21781ec1b2ce41996940d24424a9f3715dc9740d
Zofyan <s.aarrass@student.vu.nl>
2023-02-05 19:49:07 +0100

    helm3 -> helm

--- init_helm.sh
+++ init_helm.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" = "no_microk8s" ]; then
 	helm install chart chart
+    kubectl get secret root-secret -o jsonpath='{.data}' | cut -c12-771 | base64 -d > root_ca.pem
 else
 	microk8s helm3 install chart chart
+	microk8s kubectl get secret root-secret -o jsonpath='{.data}' | cut -c12-771 | base64 -d > root_ca.pem
 fi
